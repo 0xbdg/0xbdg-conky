4229964818b9dedda8237af800ef71ff3f3c4788
0xbdg <benjamindguntara@protonmail.com>
2025-12-31 13:11:59 +0800

    psadaspkd

--- scripts/mon.lua
+++ scripts/mon.lua
@@ -1,22 +1,68 @@
 require('cairo')
 require('cairo_xlib')
 
+-- draw hexagon
+function draw_hexagon(cr, cx, cy, size)
+    local angle = math.pi / 3
+    cairo_move_to(cr, cx + size, cy)
+
+    for i = 1, 6 do
+        cairo_line_to(
+            cr,
+            cx + size * math.cos(i * angle),
+            cy + size * math.sin(i * angle)
+        )
+    end
+    cairo_close_path(cr)
+end
+
+-- draw filled hexagon by percentage
+function draw_hex_percent(cr, cx, cy, size, percent, r, g, b, a)
+    cairo_set_source_rgba(cr, r, g, b, a)
+    cairo_set_line_width(cr, 6)
+
+    draw_hexagon(cr, cx, cy, size)
+    cairo_stroke_preserve(cr)
+
+    cairo_clip(cr)
+    cairo_rectangle(cr, cx - size, cy + size - (2 * size * percent), size * 2, size * 2)
+    cairo_fill(cr)
+    cairo_reset_clip(cr)
+end
+
 function conky_main()
     if conky_window == nil then return end
-    local cs = cairo_xlib_surface_create(conky_window.display,
-              conky_window.drawable,
-              conky_window.visual,
-              conky_window.width,
-              conky_window.height)
+
+    local cs = cairo_xlib_surface_create(
+        conky_window.display,
+        conky_window.drawable,
+        conky_window.visual,
+        conky_window.width,
+        conky_window.height
+    )
     local cr = cairo_create(cs)
 
-    cairo_set_source_rgba(cr, 0, 1, 0, 0.4)
-    cairo_rectangle(cr, 0, 0, 200, 30)
-    cairo_fill(cr)
+    -- CPU & RAM values
+    local cpu = tonumber(conky_parse("${cpu}")) / 100
+    local mem = tonumber(conky_parse("${memperc}")) / 100
 
-    cairo_set_source_rgba(cr, 2, 3, 0, 0.4)
-    cairo_rectangle(cr, 4, 4, 200, 30)
-    cairo_fill(cr)
+    -- CPU hex
+    draw_hex_percent(cr, 150, 120, 60, cpu, 0.2, 0.8, 1.0, 0.9)
+
+    -- Memory hex
+    draw_hex_percent(cr, 150, 230, 60, mem, 1.0, 0.4, 0.4, 0.9)
+
+    draw_hex_percent(cr, 150, 230, 60, mem, 1.0, 0.4, 0.4, 0.9)
+
+    -- Labels
+    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD)
+    cairo_set_font_size(cr, 14)
+
+    cairo_move_to(cr, 120, 120)
+    cairo_show_text(cr, "CPU")
+
+    cairo_move_to(cr, 105, 230)
+    cairo_show_text(cr, "MEM")
 
     cairo_destroy(cr)
     cairo_surface_destroy(cs)
